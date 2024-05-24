7b772a8cca616317c3d5e874e0bcca7a00e649f0
Dat Nguyen <datn1101@gmail.com>
2024-05-24 19:00:01 -0400

    Add Task components and connect to API

--- todo-app/client/src/App.js
+++ todo-app/client/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import AddTask from './components/AddTask';
+import TaskList from './components/TaskList';
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (task) => {
+    setTasks([...tasks, task]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>To-Do List</h1>
+      <AddTask onAdd={addTask} />
+      <TaskList tasks={tasks} />
     </div>
   );
-}
+};
 
 export default App;
